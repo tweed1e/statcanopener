72cd4c3e0ca4c9166eb0a3f38643c5946a486d65
Jesse Tweedle <jesse.tweedle@gmail.com>
2020-12-20 15:15:03 -0500

    Add utils todo

--- R/utils.R
+++ R/utils.R
@@ -4,3 +4,5 @@ stc_time <- function(date) {
   strftime(date, "%Y-%m-%dT%H:%M", tz = "America/Toronto")
 }
 
+# add fumctioms that check product id, vector id, etc. arguments
+# and return errors if they're wrong
